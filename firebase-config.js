4c3d858f607ff0d7ac268c59601d98e6e74b44e3
demosprojects <kentuckyr2@gmail.com>
2025-03-22 01:16:32 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,16 @@ import {
     addDoc, 
     getDocs, 
     onSnapshot, 
+    deleteDoc, // Agrega deleteDoc
+    doc, // Agrega doc
+    updateDoc, // Agrega updateDoc
     serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 import { 
     getAuth, 
     onAuthStateChanged, 
     signOut, 
-    signInWithEmailAndPassword // Agrega esta importación
+    signInWithEmailAndPassword 
 } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 
 const firebaseConfig = {
@@ -22,12 +25,13 @@ const firebaseConfig = {
   messagingSenderId: "918025008393",
   appId: "1:918025008393:web:f2c3017629b7d2a4310fd8"
 };
+
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// Exporta las funciones necesarias, incluyendo signInWithEmailAndPassword
+// Exporta las funciones necesarias
 export { 
     db, 
     auth, 
@@ -35,8 +39,11 @@ export {
     addDoc, 
     getDocs, 
     onSnapshot, 
+    deleteDoc, // Exporta deleteDoc
+    doc, // Exporta doc
+    updateDoc, // Exporta updateDoc
     serverTimestamp, 
     onAuthStateChanged, 
     signOut,
-    signInWithEmailAndPassword // Exporta signInWithEmailAndPassword
+    signInWithEmailAndPassword 
 };
